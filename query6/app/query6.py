a7fb7869677d8580d8a2480e152656cf768d1386
mar <gim.marcello@gmail.com>
2021-03-05 08:48:07 +0100

    fixed data-sender as a server

--- query6/app/query6.py
+++ query6/app/query6.py
@@ -4,8 +4,15 @@
 """
 from pyspark.streaming import StreamingContext
 from pyspark import SparkContext
+import operator
 import pymongo
 import json
+
+# Indexes in csv
+date = 0
+state = 1
+hospitalized_cum = 9
+
 
 with open("conf.json", "r") as conf_file:
     conf = json.loads(conf_file.read())
@@ -15,6 +22,11 @@ parallelismDegree = "local[%d]" % conf["parallelismDegree"]
 batchInterval = conf["batchInterval"]
 
 
+def month_from_date(fmt_date):
+    # date fmt is yyyymmdd
+    return fmt_date[4:6]
+
+
 def send_rdd(rdd_partition):
     db_conf_str = "mongodb://%s:%d" % (conf["dbAddr"], conf["dbPort"])
     collection = pymongo.MongoClient(db_conf_str)[conf["dbName"]][conf["dbCollection"]]
@@ -22,12 +34,31 @@ def send_rdd(rdd_partition):
         collection.insert_one({"location": elem[0], "day": elem[1]})
 
 
+def validate_int(n):
+    try:
+        int(n)
+    except ValueError:
+        return False
+    return True
+
+
 def main():
     ctx = SparkContext(parallelismDegree, appName)
     sctx = StreamingContext(ctx, batchInterval)
 
-    data = sctx.socketTextStream(hostname="localhost", port=conf["recv-port"])
-    
+    data = sctx.socketTextStream(hostname="localhost", port=conf["recv-port"])\
+        .map(lambda line: line.split(","))\
+        .map(lambda splitted: ((splitted[state], month_from_date(splitted[date])), splitted[hospitalized_cum]))\
+        .filter(lambda line_tuple: validate_int(line_tuple[1]))\
+        .map(lambda line_tuple: (line_tuple[0], int(line_tuple[1])))\
+        .reduceByKey(operator.add)
+
+    # foreachrdd
+
+    data.pprint()
+    sctx.start()
+    sctx.awaitTermination()
+
 
 if __name__ == "__main__":
     main()
